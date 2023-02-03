70e8b48c189445b984045a8d1ddb63393a286fd0
Daniel <daniilterawars@gmail.com>
2023-02-04 01:35:18 +0300

    fix: eslint

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,26 +1,22 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BuildOptions } from './types/config'
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.css$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      'css-loader',
+      'postcss-loader',
+    ],
+  }
 
-    const cssLoader = {
-        test: /\.css$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-        ]
-    }
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
-
-    return [
-        typescriptLoader,
-        cssLoader,
-    ]
+  return [typescriptLoader, cssLoader]
 }
