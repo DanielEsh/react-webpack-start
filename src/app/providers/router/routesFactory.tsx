5c40983f311fd16914cb718dbf97e59ce6e30a2f
Daniel <daniilterawars@gmail.com>
2023-02-19 18:53:39 +0300

    feat: not found page

--- src/app/providers/router/routesFactory.tsx
+++ src/app/providers/router/routesFactory.tsx
@@ -2,15 +2,19 @@ import { RouteProps } from 'react-router-dom'
 
 import { AboutPageAsync } from 'pages/AboutPage/AboutPage.async'
 import { MainPage } from 'pages/MainPage/MainPage'
+import { NotFoundPage } from 'pages/NotFound'
 
 export enum Routes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'notFound',
 }
 
 export const RoutePath: Record<Routes, string> = {
   [Routes.MAIN]: '/',
   [Routes.ABOUT]: 'about',
+  // should be last
+  [Routes.NOT_FOUND]: '*',
 }
 
 export const routesConfig: Record<Routes, RouteProps> = {
@@ -21,5 +25,9 @@ export const routesConfig: Record<Routes, RouteProps> = {
   [Routes.ABOUT]: {
     path: RoutePath.about,
     element: <AboutPageAsync />,
+  },
+  [Routes.NOT_FOUND]: {
+    path: RoutePath.notFound,
+    element: <NotFoundPage />,
   },
 }
---
+++ src/pages/NotFound/index.ts
@@ -0,0 +1,1 @@
+export * from 'pages/NotFound/ui/NotFoundPage'
