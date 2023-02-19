2099e1e1cc17daedada2f063cb92c2feb78d769d
Daniel <daniilterawars@gmail.com>
2023-02-19 21:36:46 +0300

    feat(table): init

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import Counter from 'components/Counter'
 import { ChangeLangButton } from 'components/ChangeLangButton'
+import { Table } from 'shared/ui/Table'
 
 export const MainPage = () => {
   return (
     <div>
       <h1>MainPage</h1>
+      <Table />
       <Counter />
       <ChangeLangButton />
     </div>
