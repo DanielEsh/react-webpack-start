373fdf1c6b899ba0c30315a94071400ea0800104
Daniel <daniilterawars@gmail.com>
2023-03-03 05:59:20 +0300

    feat: collections page

--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -1,6 +1,8 @@
 import { RouteProps } from 'react-router-dom'
 import { AboutPageAsync } from 'pages/AboutPage/AboutPage.async'
 import { MainPage } from 'pages/MainPage/MainPage'
+import CollectionsPage from 'pages/CollectionsPage/CollectionsPage'
+import CollectionPage from 'pages/CollectionPage/CollectionPage'
 import { NotFoundPage } from 'pages/NotFound'
 
 export type AppRoutesProps = RouteProps & {
@@ -10,6 +12,8 @@ export type AppRoutesProps = RouteProps & {
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  COLLECTIONS = 'collections',
+  COLLECTION = 'collection',
   // should be last
   NOT_FOUND = 'not_found',
 }
@@ -17,6 +21,8 @@ export enum AppRoutes {
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.COLLECTIONS]: '/collections',
+  [AppRoutes.COLLECTION]: '/collections/', // + :id
   // should be last
   [AppRoutes.NOT_FOUND]: '*',
 }
@@ -29,6 +35,16 @@ export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
   [AppRoutes.ABOUT]: {
     path: RoutePath.about,
     element: <AboutPageAsync />,
+    authOnly: true,
+  },
+  [AppRoutes.COLLECTIONS]: {
+    path: RoutePath.collections,
+    element: <CollectionsPage />,
+    authOnly: true,
+  },
+  [AppRoutes.COLLECTION]: {
+    path: `${RoutePath.collection}:id`,
+    element: <CollectionPage />,
     authOnly: true,
   },
   // should be last
