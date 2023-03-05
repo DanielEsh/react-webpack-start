474841e0a0431a6318328609058ad686087383a8
Daniel <daniilterawars@gmail.com>
2023-03-05 23:24:25 +0300

    feat(mocks): pagination

--- src/__mocks__/collection/db.ts
+++ src/__mocks__/collection/db.ts
@@ -22,7 +22,27 @@ for (let i = 0; i < ITEMS_COUNT; i++) db.collection.create()
 
 const readAll = () => {
   return rest.get('http://localhost:8000/api/collection', (_req, res, ctx) => {
-    const result = db.collection.getAll()
+    const firstPage = db.collection.findMany({
+      take: 2,
+      cursor: null,
+    })
+
+    const secondPage = db.collection.findMany({
+      take: 2,
+      cursor: firstPage[firstPage.length - 1].id,
+    })
+
+    const items = db.collection.findMany({
+      take: 2,
+      skip: 2,
+    })
+
+    const result = {
+      ...items,
+      totalCount: db.collection.count(),
+      firstPage: firstPage,
+      secondPage: secondPage,
+    }
 
     return res(ctx.json(result))
   })
