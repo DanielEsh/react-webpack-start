3c2c182b24cf7a35f9c0c36cb2749d9c91f50d1d
Daniel <daniilterawars@gmail.com>
2023-03-08 16:07:07 +0300

    feat(Pagination): getPaginationModel

--- src/shared/ui/Pagiantion/Pagination.tsx
+++ src/shared/ui/Pagiantion/Pagination.tsx
@@ -1,4 +1,9 @@
-import { usePagination } from 'shared/lib/hooks/usePagination'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+// import { usePagination } from 'shared/lib/hooks/usePagination'
+import { paginationFactory } from 'shared/ui/Pagiantion/getPaginationModel'
+import { clsx } from 'clsx'
 
 interface Props {
   currentPage: number
@@ -6,13 +11,137 @@ interface Props {
   onChange?: (item: string) => void
 }
 
+type PaginationItemsProps = {
+  disabled: boolean
+  onClick: () => void
+}
+
+type PaginationPageProps = {
+  isActive: boolean
+  disabled: boolean
+  value: number
+  onClick: () => void
+}
+
+const pagesClasses = (isActive: boolean) =>
+  clsx('flex items-center justify-center w-8 h-8 border border-black', {
+    ['bg-black text-white']: isActive,
+  })
+
+const itemsClasses = (interactive = true) =>
+  clsx('flex items-center justify-center w-8 h-8 border border-black', {
+    ['opacity-60']: !interactive,
+  })
+
+export const FirstPageLink = ({ onClick, disabled }: PaginationItemsProps) => (
+  <li>
+    <button
+      className={itemsClasses(disabled)}
+      disabled={disabled}
+      onClick={onClick}>
+      FIRST
+    </button>
+  </li>
+)
+
+export const PreviousPageLink = ({
+  onClick,
+  disabled,
+}: PaginationItemsProps) => (
+  <li>
+    <button
+      className={itemsClasses(disabled)}
+      disabled={disabled}
+      onClick={onClick}>
+      PREV
+    </button>
+  </li>
+)
+
+export const NextPageLink = ({ onClick, disabled }: PaginationItemsProps) => (
+  <li>
+    <button
+      className={itemsClasses(disabled)}
+      disabled={disabled}
+      onClick={onClick}>
+      NEXT
+    </button>
+  </li>
+)
+
+export const LastPageLink = ({ onClick, disabled }: PaginationItemsProps) => (
+  <li>
+    <button
+      className={itemsClasses(disabled)}
+      disabled={disabled}
+      onClick={onClick}>
+      LAST
+    </button>
+  </li>
+)
+
+export const Ellipsis = ({ onClick, disabled }: PaginationItemsProps) => (
+  <li>
+    <button
+      className={itemsClasses(disabled)}
+      disabled={disabled}
+      onClick={onClick}>
+      ...
+    </button>
+  </li>
+)
+
+export const PageLink = ({
+  isActive,
+  value,
+  disabled,
+  onClick,
+}: PaginationPageProps) => (
+  <li>
+    <button
+      className={pagesClasses(isActive)}
+      onClick={onClick}
+      disabled={disabled}>
+      {value}
+    </button>
+  </li>
+)
+
+const renderItemComponentFunctionFactory = (
+  itemTypeToComponent,
+  currentPage,
+  onChange,
+) => {
+  const onItemClickFunctionFactory = ({ value, isDisabled }) => {
+    return () => {
+      if (!isDisabled && onChange && currentPage !== value) {
+        onChange(value)
+      }
+    }
+  }
+
+  return (props) => {
+    const ItemComponent = itemTypeToComponent[props.type]
+    const onItemClick = onItemClickFunctionFactory(props)
+    return <ItemComponent onClick={onItemClick} {...props} />
+  }
+}
+
 export const Pagiantion = (props: Props) => {
   const { currentPage, totalPages, onChange } = props
+  const disabled = false
+  // const paginationModel = usePagination({
+  //   currentPage: currentPage,
+  //   totalPagesCount: totalPages,
+  // })
 
-  const paginationModel = usePagination({
+  const paginationModel = paginationFactory({
+    boundaryPagesRange: 0,
+    totalPages: totalPages,
     currentPage: currentPage,
-    totalPagesCount: totalPages,
   })
+
+  console.log('MODEL', paginationModel)
 
   const handlePageClick = (item: string) => {
     if (!onChange) return
@@ -21,22 +150,29 @@ export const Pagiantion = (props: Props) => {
 
   const buttonClasses = 'border border-stone-800 py-1 px-2'
 
+  const itemTypeToComponent = {
+    PAGE: PageLink,
+    ELLIPSIS: Ellipsis,
+    FIRST_PAGE_LINK: FirstPageLink,
+    PREVIOUS_PAGE_LINK: PreviousPageLink,
+    NEXT_PAGE_LINK: NextPageLink,
+    LAST_PAGE_LINK: LastPageLink,
+  }
+
+  const renderItemComponent = renderItemComponentFunctionFactory(
+    itemTypeToComponent,
+    currentPage,
+    onChange,
+  )
+
   return (
-    <div className="flex gap-3">
-      <div className={buttonClasses}>First</div>
-      <div className={buttonClasses}>Prev</div>
-      {paginationModel?.map((item: any, idx: number) => {
-        return (
-          <div
-            className={buttonClasses}
-            key={idx}
-            onClick={() => handlePageClick(item)}>
-            {item}
-          </div>
-        )
-      })}
-      <div className={buttonClasses}>Next</div>
-      <div className={buttonClasses}>Last</div>
-    </div>
+    <ul className="mt-6 flex gap-3">
+      {paginationModel.map((itemModel) =>
+        renderItemComponent({
+          ...itemModel,
+          isDisabled: !!disabled,
+        }),
+      )}
+    </ul>
   )
 }
---
+++ src/shared/ui/Pagiantion/getPaginationModel.ts
@@ -0,0 +1,172 @@
+export const createRange = (start: number, end: number): number[] => {
+  const range: number[] = []
+  for (let i = start; i <= end; i++) {
+    range.push(i)
+  }
+  return range
+}
+
+export const createPageFactory = (currentPage: number) => {
+  return (pageNumber: number) => {
+    return {
+      type: 'PAGE',
+      key: pageNumber,
+      value: pageNumber,
+      isActive: pageNumber === currentPage,
+    }
+  }
+}
+
+export const createFirstEllipsis = (pageNumber: number) => {
+  return {
+    type: 'ELLIPSIS',
+    key: 'FIRST_ELLIPSIS',
+    value: pageNumber,
+    isActive: false,
+  }
+}
+
+export const createSecondEllipsis = (pageNumber: number) => {
+  return {
+    type: 'ELLIPSIS',
+    key: 'SECOND_ELLIPSIS',
+    value: pageNumber,
+    isActive: false,
+  }
+}
+
+export const createPreviousPageLink = (currentPage: number) => {
+  return {
+    type: 'PREVIOUS_PAGE_LINK',
+    key: 'PREVIOUS_PAGE_LINK',
+    value: Math.max(1, currentPage - 1),
+    isActive: currentPage === 1,
+  }
+}
+
+export const createNextPageLink = (currentPage: number, totalPages: number) => {
+  return {
+    type: 'NEXT_PAGE_LINK',
+    key: 'NEXT_PAGE_LINK',
+    value: Math.min(totalPages, currentPage + 1),
+    isActive: currentPage === totalPages,
+  }
+}
+
+export const createFirstPageLink = (currentPage: number) => {
+  return {
+    type: 'FIRST_PAGE_LINK',
+    key: 'FIRST_PAGE_LINK',
+    value: 1,
+    isActive: currentPage === 1,
+  }
+}
+
+export const createLastPageLink = (currentPage: number, totalPages: number) => {
+  return {
+    type: 'LAST_PAGE_LINK',
+    key: 'LAST_PAGE_LINK',
+    value: totalPages,
+    isActive: currentPage === totalPages,
+  }
+}
+
+interface PaginationModelOptions {
+  /**
+   * total number of pages
+   */
+  totalPages: number
+
+  /**
+   * currentPage number
+   */
+  currentPage: number
+
+  /**
+   * number of always visible pages at the beginning and end
+   */
+  boundaryPagesRange?: number
+
+  /**
+   * number of always visible pages before and after the current one
+   */
+  siblingPagesRange?: number
+}
+
+const DEFAULT_SIBLING_COUNT = 1
+const DEFAULT_BOUNDARY_PAGES_RANGE = 1
+
+export function paginationFactory(options: PaginationModelOptions) {
+  const {
+    boundaryPagesRange = DEFAULT_BOUNDARY_PAGES_RANGE,
+    siblingPagesRange = DEFAULT_SIBLING_COUNT,
+    totalPages,
+    currentPage,
+  } = options
+
+  const ellipsisSize = 1
+  const paginationModel = []
+  const createPage = createPageFactory(currentPage)
+
+  // Simplify generation of pages if number of available items is equal or greater than total pages to show
+  if (
+    1 + 2 * ellipsisSize + 2 * siblingPagesRange + 2 * boundaryPagesRange >=
+    totalPages
+  ) {
+    const allPages = createRange(1, totalPages).map(createPage)
+    paginationModel.push(...allPages)
+  } else {
+    // Calculate group of first pages
+    const firstPagesStart = 1
+    const firstPagesEnd = boundaryPagesRange
+    const firstPages = createRange(firstPagesStart, firstPagesEnd).map(
+      createPage,
+    )
+
+    // Calculate group of last pages
+    const lastPagesStart = totalPages + 1 - boundaryPagesRange
+    const lastPages = createRange(lastPagesStart, totalPages).map(createPage)
+
+    // Calculate group of main pages
+    const mainPagesStart = Math.min(
+      Math.max(
+        currentPage - siblingPagesRange,
+        firstPagesEnd + ellipsisSize + 1,
+      ),
+      lastPagesStart - ellipsisSize - 2 * siblingPagesRange - 1,
+    )
+    const mainPagesEnd = mainPagesStart + 2 * siblingPagesRange
+    const mainPages = createRange(mainPagesStart, mainPagesEnd).map(createPage)
+
+    // Add group of first pages
+    paginationModel.push(...firstPages)
+
+    // Calculate and add ellipsis before group of main pages
+    const firstEllipsisPageNumber = mainPagesStart - 1
+    const showPageInsteadOfFirstEllipsis =
+      firstEllipsisPageNumber === firstPagesEnd + 1
+    const createFirstEllipsisOrPage = showPageInsteadOfFirstEllipsis
+      ? createPage
+      : createFirstEllipsis
+    const firstEllipsis = createFirstEllipsisOrPage(firstEllipsisPageNumber)
+    paginationModel.push(firstEllipsis)
+
+    // Add group of main pages
+    paginationModel.push(...mainPages)
+
+    // Calculate and add ellipsis after group of main pages
+    const secondEllipsisPageNumber = mainPagesEnd + 1
+    const showPageInsteadOfSecondEllipsis =
+      secondEllipsisPageNumber === lastPagesStart - 1
+    const createSecondEllipsisOrPage = showPageInsteadOfSecondEllipsis
+      ? createPage
+      : createSecondEllipsis
+    const secondEllipsis = createSecondEllipsisOrPage(secondEllipsisPageNumber)
+    paginationModel.push(secondEllipsis)
+
+    // Add group of last pages
+    paginationModel.push(...lastPages)
+  }
+
+  return paginationModel
+}
