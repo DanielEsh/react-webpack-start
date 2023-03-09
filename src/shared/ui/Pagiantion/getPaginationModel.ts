707fe725b7e8d4e684f2ee25117de296379a6ed2
Daniel <daniilterawars@gmail.com>
2023-03-09 22:32:34 +0300

    refactor(Pagination): getPaginationModel

--- src/shared/ui/Pagiantion/getPaginationModel.ts
+++ src/shared/ui/Pagiantion/getPaginationModel.ts
@@ -1,73 +1,13 @@
 import {
-  PaginationElementsType,
-  createPaginationElement,
   createRange,
+  createFirstEllipsis,
+  createSecondEllipsis,
+  createPreviousPageLink,
+  createNextPageLink,
+  createLastPageLink,
+  createFirstPageLink,
+  createPageFactory,
 } from './helpers'
-
-export const createPageFactory = (currentPage: number) => {
-  return (pageNumber: number) => {
-    return createPaginationElement({
-      type: PaginationElementsType.PAGE,
-      key: pageNumber,
-      value: pageNumber,
-      isActive: pageNumber === currentPage,
-    })
-  }
-}
-
-export const createFirstEllipsis = (pageNumber: number) => {
-  return {
-    type: 'ELLIPSIS',
-    key: 'FIRST_ELLIPSIS',
-    value: pageNumber,
-    isActive: false,
-  }
-}
-
-export const createSecondEllipsis = (pageNumber: number) => {
-  return {
-    type: 'ELLIPSIS',
-    key: 'SECOND_ELLIPSIS',
-    value: pageNumber,
-    isActive: false,
-  }
-}
-
-export const createPreviousPageLink = (currentPage: number) => {
-  return {
-    type: 'PREVIOUS_PAGE_LINK',
-    key: 'PREVIOUS_PAGE_LINK',
-    value: Math.max(1, currentPage - 1),
-    isActive: currentPage === 1,
-  }
-}
-
-export const createNextPageLink = (currentPage: number, totalPages: number) => {
-  return {
-    type: 'NEXT_PAGE_LINK',
-    key: 'NEXT_PAGE_LINK',
-    value: Math.min(totalPages, currentPage + 1),
-    isActive: currentPage === totalPages,
-  }
-}
-
-export const createFirstPageLink = (currentPage: number) => {
-  return {
-    type: 'FIRST_PAGE_LINK',
-    key: 'FIRST_PAGE_LINK',
-    value: 1,
-    isActive: currentPage === 1,
-  }
-}
-
-export const createLastPageLink = (currentPage: number, totalPages: number) => {
-  return {
-    type: 'LAST_PAGE_LINK',
-    key: 'LAST_PAGE_LINK',
-    value: totalPages,
-    isActive: currentPage === totalPages,
-  }
-}
 
 interface PaginationModelOptions {
   /**
