586f70dd5972fbece96f080b65880ceb6d673b3e
Daniel <daniilterawars@gmail.com>
2023-03-11 20:15:31 +0300

    refactor(Pagination): elements

--- src/shared/ui/Pagiantion/PaginationElements.tsx
+++ src/shared/ui/Pagiantion/PaginationElements.tsx
@@ -1,100 +1,64 @@
+import type { ReactNode } from 'react'
 import { clsx } from 'clsx'
+import { PaginationElementsType } from './types'
 
 interface PaginationElementProps {
   isActive: boolean
   disabled: boolean
-  value: number
+  value: number | ReactNode
   onClick: () => void
 }
 
-const pagesClasses = (isActive: boolean) =>
-  clsx('flex items-center justify-center w-8 h-8 border border-black', {
-    ['bg-black text-white']: isActive,
-  })
-
-const itemsClasses = (interactive = true) =>
-  clsx('flex items-center justify-center w-8 h-8 border border-black', {
-    ['opacity-60']: !interactive,
-  })
-
-type PaginationItemsProps = {
+interface PaginationItemsProps {
   disabled: boolean
   onClick: () => void
 }
 
-export const PageLink = ({
-  isActive,
-  value,
-  disabled,
-  onClick,
-}: PaginationElementProps) => (
-  <li>
-    <button
-      className={pagesClasses(isActive)}
-      onClick={onClick}
-      disabled={disabled}>
-      {value}
-    </button>
-  </li>
+const pagesClasses = (isActive: boolean) =>
+  clsx('flex items-center justify-center py-1 px-2 border border-black', {
+    ['bg-black text-white']: isActive,
+  })
+
+const itemsClasses = (interactive = true) =>
+  clsx('flex items-center justify-center py-1 px-2 border border-black', {
+    ['opacity-60']: !interactive,
+  })
+
+const PaginationElement = (props: PaginationElementProps) => {
+  const { value, disabled, isActive = false, onClick = () => null } = props
+
+  return (
+    <li>
+      <button
+        className={pagesClasses(isActive)}
+        onClick={onClick}
+        disabled={disabled}>
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export const PageLink = (props: PaginationElementProps) => (
+  <PaginationElement {...props} />
 )
 
-export const FirstPageLink = ({
-  onClick,
-  disabled,
-}: PaginationElementProps) => (
-  <li>
-    <button
-      className={itemsClasses(disabled)}
-      disabled={disabled}
-      onClick={onClick}>
-      FIRST
-    </button>
-  </li>
-)
+export const FirstPageLink = (props: PaginationElementProps) => {
+  return <PaginationElement {...props} value="FIRST" isActive={false} />
+}
 
-export const PreviousPageLink = ({
-  onClick,
-  disabled,
-}: PaginationElementProps) => (
-  <li>
-    <button
-      className={itemsClasses(disabled)}
-      disabled={disabled}
-      onClick={onClick}>
-      PREV
-    </button>
-  </li>
-)
+export const PreviousPageLink = (props: PaginationElementProps) => {
+  return <PaginationElement {...props} value="PREVIOUS" isActive={false} />
+}
 
-export const NextPageLink = ({ onClick, disabled }: PaginationElementProps) => (
-  <li>
-    <button
-      className={itemsClasses(disabled)}
-      disabled={disabled}
-      onClick={onClick}>
-      NEXT
-    </button>
-  </li>
-)
+export const NextPageLink = (props: PaginationElementProps) => {
+  return <PaginationElement {...props} value="NEXT" isActive={false} />
+}
 
-export const LastPageLink = ({ onClick, disabled }: PaginationElementProps) => (
-  <li>
-    <button
-      className={itemsClasses(disabled)}
-      disabled={disabled}
-      onClick={onClick}>
-      LAST
-    </button>
-  </li>
-)
+export const LastPageLink = (props: PaginationElementProps) => {
+  return <PaginationElement {...props} value="LAST" isActive={false} />
+}
 
-export const Ellipsis = ({ onClick, disabled }: PaginationItemsProps) => (
-  <li>
-    <button
-      className={itemsClasses(disabled)}
-      disabled={disabled}
-      onClick={onClick}>
-      ...
-    </button>
-  </li>
-)
+export const Ellipsis = (props: PaginationElementProps) => {
+  return <PaginationElement {...props} value="..." isActive={false} />
+}
