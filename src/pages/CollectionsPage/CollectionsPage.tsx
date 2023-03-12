d075c932f64b788f979f5ef67aaa72a89422cb29
Daniel <daniilterawars@gmail.com>
2023-03-12 20:11:32 +0300

    feat(pagination): select

--- src/pages/CollectionsPage/CollectionsPage.tsx
+++ src/pages/CollectionsPage/CollectionsPage.tsx
@@ -4,10 +4,14 @@ import { getTestData } from 'shared/api/api'
 import { useSearchParams } from 'react-router-dom'
 import { Pagiantion } from 'shared/ui/Pagiantion/Pagination'
 
+type RowsPerPage = 2 | 5 | 10
+
 const CollectionsPage = () => {
   const [testData, setTestData] = useState<any>(null)
   const [isLoading, setLoading] = useState(true)
   const [isError, setIsError] = useState(false)
+
+  const [rowsPerPage, setRowsPerPage] = useState<RowsPerPage>(2)
 
   const [searchParams, setSearchParams] = useSearchParams()
 
@@ -35,6 +39,11 @@ const CollectionsPage = () => {
     }, 2000)
   }, [currentPage, limit])
 
+  const handleSelectRowsChange = (event: any) => {
+    setRowsPerPage(event.target.value)
+    setSearchParams({ limit: event.target.value })
+  }
+
   const beautifyRender = () => {
     return testData.items.map((item: any, idx: number) => {
       return <div key={idx}>{JSON.stringify(item, null, '\t')}</div>
@@ -53,11 +62,26 @@ const CollectionsPage = () => {
       <>
         {beautifyRender()}
         {testData && (
-          <Pagiantion
-            currentPage={parseInt(currentPage)}
-            totalPages={testData.meta.totalPages}
-            onChange={(item) => handlePageClick(item)}
-          />
+          <div className="mt-6 flex gap-3">
+            <label>
+              Rows per Page:
+              <select
+                name="select"
+                value={rowsPerPage}
+                onChange={handleSelectRowsChange}
+              >
+                <option value="2">2</option>
+                <option value="5">5</option>
+                <option value="10">10</option>
+              </select>
+            </label>
+
+            <Pagiantion
+              currentPage={parseInt(currentPage)}
+              totalPages={testData.meta.totalPages}
+              onChange={(item) => handlePageClick(item)}
+            />
+          </div>
         )}
       </>
     )
