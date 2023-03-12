fb883427e0f1b02d08f7a4f8ff878ae07ce6b27c
Daniel <daniilterawars@gmail.com>
2023-03-12 17:33:22 +0300

    feat: env

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,12 @@
 import webpack from 'webpack'
 import path from 'path'
+import dotenv from 'dotenv'
 import { buildWebpackConfig } from './config/build/buildWebpackConfig'
 import { BuildEnv, BuildPaths } from './config/build/types/config'
+
+const DEFAULT_PORT = 3000
+
+dotenv.config()
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
@@ -11,9 +16,12 @@ export default (env: BuildEnv) => {
     dirSrc: path.resolve(__dirname, 'src'),
   }
 
+  console.log('ENV', process.env.PORT)
+  const envPort = process.env.PORT as number | undefined
+
   const mode = env.mode || 'development'
   const isDev = mode === 'development'
-  const PORT = env.port || 3000
+  const PORT = envPort ?? DEFAULT_PORT
 
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
