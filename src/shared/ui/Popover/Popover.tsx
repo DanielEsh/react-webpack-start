636fb2d723cacba5d7deb1970855e8bf3b631a48
Daniel <daniilterawars@gmail.com>
2023-03-21 00:49:18 +0300

    feat(Popover): offset

--- src/shared/ui/Popover/Popover.tsx
+++ src/shared/ui/Popover/Popover.tsx
@@ -1,20 +1,41 @@
 import { useState } from 'react'
-import { useFloating } from '@floating-ui/react-dom'
+import { useFloating, offset as floatingOffset } from '@floating-ui/react-dom'
 import type { Placement } from '@floating-ui/react-dom'
 import { Portal } from 'shared/ui/Portal/Portal'
 
+type Offset = {
+  side: number
+  align: number
+}
+
 export interface PopoverProps {
   placement: Placement
+  offset?: Offset
+}
+
+function isDefined<T>(value: T | undefined): value is T {
+  return value !== undefined
+}
+
+const DEFAULT_OFFSET: Offset = {
+  side: 10,
+  align: 10,
 }
 
 export const Popover = (props: PopoverProps) => {
-  const { placement } = props
+  const { placement, offset = DEFAULT_OFFSET } = props
 
   const [isOpen, setOpen] = useState(false)
 
   const { reference, floating, strategy, x, y } = useFloating({
     strategy: 'fixed',
     placement,
+    middleware: [
+      floatingOffset({
+        mainAxis: offset.side,
+        alignmentAxis: offset.align,
+      }),
+    ].filter(isDefined),
   })
 
   return (
