fee020e57f124555aebdeba3cf24957af0ca586e
Daniel <daniilterawars@gmail.com>
2023-03-21 00:30:40 +0300

    feat(Popover): open

--- src/shared/ui/Popover/Popover.tsx
+++ src/shared/ui/Popover/Popover.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import { useFloating } from '@floating-ui/react-dom'
 
 export const Popover = () => {
+  const [isOpen, setOpen] = useState(false)
+
   const { reference, floating, strategy, x, y } = useFloating({
     strategy: 'fixed',
     placement: 'top',
@@ -8,20 +11,23 @@ export const Popover = () => {
 
   return (
     <div className="mt-6">
-      <div
-        ref={floating}
-        style={{
-          position: strategy,
-          top: y ?? 0,
-          left: x ?? 0,
-          visibility: x == null ? 'hidden' : 'visible',
-        }}
-      >
-        Popover floating
-      </div>
+      {isOpen ? (
+        <div
+          ref={floating}
+          style={{
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            visibility: x == null ? 'hidden' : 'visible',
+          }}
+        >
+          Popover floating
+        </div>
+      ) : null}
       <div
         ref={reference}
         className="inline-flex"
+        onClick={() => setOpen(!isOpen)}
       >
         Popover trigger
       </div>
