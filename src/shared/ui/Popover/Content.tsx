c8e880eb982a3d6182bdd43a4acab327f44a1f49
Daniel <daniilterawars@gmail.com>
2023-03-24 22:52:43 +0300

    feat(Transition): fade

--- src/shared/ui/Popover/Content.tsx
+++ src/shared/ui/Popover/Content.tsx
@@ -2,25 +2,43 @@ import { useContext, PropsWithChildren } from 'react'
 import { Portal } from 'shared/ui/Portal/Portal'
 import { PopoverContext } from './Context'
 import { PopoverArrow } from './Arrow'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const fade = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+}
 
 export const PopoverContent = (props: PropsWithChildren) => {
   const { floatingRef, popoverStyles, isOpened } = useContext(PopoverContext)
 
   return (
-    <div>
+    <AnimatePresence>
       {isOpened ? (
         <Portal>
-          <div
+          <motion.div
             ref={floatingRef}
             className="flex rounded-md bg-neutral-800 p-2 text-white"
             style={popoverStyles}
+            variants={fade}
+            {...fade}
           >
             {props.children}
 
             <PopoverArrow />
-          </div>
+          </motion.div>
         </Portal>
       ) : null}
-    </div>
+    </AnimatePresence>
   )
 }
