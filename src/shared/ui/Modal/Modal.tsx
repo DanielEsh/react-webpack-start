f02bdcdd1204469300615b661cf9e24d650a6138
Daniel <daniilterawars@gmail.com>
2023-03-29 21:48:59 +0300

    feat(Modal): styles

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -12,7 +12,10 @@ const COMPONENT_NAME = 'Modal'
 export const Modal = (props: ModalProps) => {
   const { opened, className, children, onClose } = props
 
-  const classes = classNames('modal', className)
+  const classes = classNames(
+    'absolute top-0 right-0 w-[880px] h-full p-4 bg-white',
+    className,
+  )
 
   const handleClose = () => {
     if (!onClose) return
@@ -23,8 +26,10 @@ export const Modal = (props: ModalProps) => {
   return opened ? (
     <Portal>
       <div className="fixed inset-0 overflow-hidden bg-neutral-800/50">
-        <button onClick={handleClose}>Close</button>
-        <div className={classes}>{children}</div>
+        <div className={classes}>
+          <button onClick={handleClose}>Close</button>
+          {children}
+        </div>
       </div>
     </Portal>
   ) : null
