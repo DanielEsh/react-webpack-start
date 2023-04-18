e055e5c29ae098e0d90788d298a9ff72f1ca3767
Daniel <daniilterawars@gmail.com>
2023-04-18 21:39:26 +0300

    feat(collections): useCreateCollection

--- src/entities/Collection/api/index.ts
+++ src/entities/Collection/api/index.ts
@@ -1,9 +1,15 @@
-import { useQuery } from '@tanstack/react-query'
-import { getCollections } from 'shared/api/api'
+import { useQuery, useMutation } from '@tanstack/react-query'
+import { getCollections, createCollection } from 'shared/api/api'
 
 export const useGetCollections = () => {
   return useQuery({
     queryKey: ['collections'],
     queryFn: getCollections,
+  })
+}
+
+export const useCreateCollection = () => {
+  return useMutation({
+    mutationFn: createCollection,
   })
 }
