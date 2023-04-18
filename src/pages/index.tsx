cb1eea495751c1f900e6202fe4a3ed146fd6bbd4
Daniel <daniilterawars@gmail.com>
2023-04-18 21:51:39 +0300

    feat(collections): routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RootLayout from 'widgets/layouts/RootLayout'
 import { MainPage } from 'pages/MainPage/MainPage'
 import { AboutPageAsync } from 'pages/AboutPage/AboutPage.async'
-import CollectionsPage from 'pages/CollectionsPage/CollectionsPage'
-import CollectionPage from 'pages/CollectionPage/CollectionPage'
-import CollectionsCreate from 'pages/CollectionsCreate/CollectionsCreate'
 import { GlobalNotFoundPage } from 'pages/GlobalNotFound'
+
+import CollectionsRouting from 'pages/collection'
 
 export const Router = () => {
   return (
@@ -25,19 +24,7 @@ export const Router = () => {
             element={<AboutPageAsync />}
           />
 
-          <Route
-            path="collections"
-            element={<CollectionsPage />}
-          >
-            <Route
-              path="create"
-              element={<CollectionsCreate />}
-            />
-            <Route
-              path=":id"
-              element={<CollectionPage />}
-            />
-          </Route>
+          {...CollectionsRouting}
         </Route>
 
         <Route
