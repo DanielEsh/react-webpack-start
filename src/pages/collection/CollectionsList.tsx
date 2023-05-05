9e3b7b2ec048229a55c76cd7dd0ab33ad52905a1
Daniel <daniilterawars@gmail.com>
2023-05-05 22:41:35 +0300

    feat: qs

--- src/pages/collection/CollectionsList.tsx
+++ src/pages/collection/CollectionsList.tsx
@@ -7,8 +7,50 @@ import { CollectionsTable } from 'entities/Collection'
 type RowsPerPage = 5 | 10 | 25
 
 import { Data } from 'entities/Collection/types'
+import qs from 'qs'
+
+export function objectToQuery(object: any) {
+
+  const DELIMITER = '&'
+
+  let paramString = ''
+
+  for (const name in object) {
+    if (!Array.isArray(object[name])) {
+      paramString += `${name}=${object[name]}&`
+    } else {
+      paramString += `${name}=`
+      object[name].forEach((val: any) => {
+        if (val.toString() === 'true') {
+          paramString += '1,'
+        } else if (val.toString() === 'false') {
+          paramString += '0,'
+        } else {
+          paramString += `${val},`
+        }
+      })
+
+      paramString = paramString.slice(0, -1)
+      paramString += DELIMITER
+    }
+  }
+  paramString = paramString.slice(0, -1)
+  return paramString
+}
+
+const test = () => {
+  const obj = {
+    slug: 'asc',
+    name: 'desc',
+    limit: 5,
+    page: 2,
+  }
+
+  console.log('test', objectToQuery(obj))
+}
 
 const CollectionsPage = () => {
+  test()
   const { isLoading, isError, data } = useGetCollections()
 
   const [rowsPerPage, setRowsPerPage] = useState<RowsPerPage>(5)
@@ -45,7 +87,7 @@ const CollectionsPage = () => {
         {data && (
           <CollectionsTable
             currentPage={Number(currentPage)}
-            items={data.items}
+            items={data.data}
             meta={data.meta}
             rowPerPage={rowsPerPage}
             onPageChange={handlePageClick}
