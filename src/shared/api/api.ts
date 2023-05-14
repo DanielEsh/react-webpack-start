3d1ba291b6b020b66d2d66c01c942c725105ac28
Daniel <daniilterawars@gmail.com>
2023-05-14 14:56:31 +0300

    feat(api): contract

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,22 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { getBaseUrl } from 'shared/api'
 
 export const $api = axios.create({
   baseURL: getBaseUrl(),
 })
+
+export const defaultsHeaders = {}
+export const defaultParams = (params?: any): AxiosRequestConfig => ({
+  headers: defaultsHeaders,
+  ...params,
+})
+
+export const api = {
+  get: <T>(url: string, params?: object) =>
+    axios.get<T>(url, defaultParams(params)),
+  post: <T>(url: string, data: any) =>
+    axios.post<T>(url, data, defaultParams()),
+  patch: <T>(url: string, data: any) =>
+    axios.patch<T>(url, data, defaultParams()),
+  delete: <T>(url: string) => axios.delete<T>(url, defaultParams()),
+}
--- src/shared/api/endpoints.ts
+++
@@ -1,5 +0,0 @@
-export const apiEndpoints = {
-  collections: {
-    list: 'collections/',
-  },
-}
