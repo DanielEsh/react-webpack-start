fdbd60e57040366304ba40997647f2ae774cdc04
Daniel <daniilterawars@gmail.com>
2023-05-31 10:29:31 +0300

    feat(Toast): toasts region

--- src/shared/ui-kit/Toast/Toast.tsx
+++ src/shared/ui-kit/Toast/Toast.tsx
@@ -16,7 +16,6 @@ export const Toast = (props: Props) => {
   } = props
 
   const handleClose = () => {
-    console.log('close', index)
     hide(index)
   }
 
--- src/shared/ui-kit/Toast/ToastRoot.tsx
+++ src/shared/ui-kit/Toast/ToastRoot.tsx
@@ -5,6 +5,8 @@ import { Toast } from './Toast'
 
 const COMPONENT_NAME = 'ToastRoot'
 
+const TOAST_ROOT_ELEMENT = document.getElementById('toasts')
+
 export const ToastRoot = () => {
   const toasts = useList($notifications, (toast, index) => (
     <Toast
@@ -14,7 +16,7 @@ export const ToastRoot = () => {
   ))
 
   return (
-    <Portal>
+    <Portal container={TOAST_ROOT_ELEMENT}>
       <ol className="fixed top-[16px] right-[16px] flex flex-col gap-4">
         {toasts}
       </ol>
