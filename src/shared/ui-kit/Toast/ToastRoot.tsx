de1d0267e8eb57b9f9c7d93eb240a52583bdb6a4
Daniel <daniilterawars@gmail.com>
2023-05-31 15:53:33 +0300

    feat(Toast): auto close

--- src/shared/ui-kit/Toast/ToastRoot.tsx
+++ src/shared/ui-kit/Toast/ToastRoot.tsx
@@ -1,16 +1,42 @@
+import { useEffect, useRef } from 'react'
 import { Portal } from '../Portal'
 import { useStore } from 'effector-react'
-import { $notifications } from './event'
+import { $notifications, hide } from './event'
 import { Toast } from './Toast'
 
 const COMPONENT_NAME = 'ToastRoot'
 
 const TOAST_ROOT_ELEMENT = document.getElementById('toasts')
 
-export const ToastRoot = () => {
-  const { state: toastsList } = useStore($notifications)
+const autoCloseTimeout = 5000
 
-  const toasts = toastsList.map((toast, index) => (
+export const ToastRoot = () => {
+  const { state: toasts } = useStore($notifications)
+
+  const hideTimeout = useRef<number>()
+
+  const handleHide = () => {
+    hide(0)
+    window.clearTimeout(hideTimeout.current)
+    if (toasts.length) {
+      handleDelayedHide()
+    }
+  }
+
+  const cancelDelayedHide = () => {
+    clearTimeout(hideTimeout.current)
+  }
+
+  const handleDelayedHide = () => {
+    hideTimeout.current = window.setTimeout(handleHide, autoCloseTimeout)
+  }
+
+  useEffect(() => {
+    handleDelayedHide()
+    return cancelDelayedHide
+  }, [])
+
+  const renderToasts = toasts.map((toast, index) => (
     <Toast
       index={index}
       toast={toast}
@@ -20,7 +46,7 @@ export const ToastRoot = () => {
   return (
     <Portal container={TOAST_ROOT_ELEMENT}>
       <ol className="fixed top-[16px] right-[16px] flex flex-col gap-4">
-        {toasts}
+        {renderToasts}
       </ol>
     </Portal>
   )
