e28eb005078e3e13040604f67ed7b505ed5aa329
Daniel <daniilterawars@gmail.com>
2023-07-08 10:19:45 +0300

    feat: useQueue

---
+++ src/shared/ui-kit/Toast/useQueue.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+interface useQueueArgs<T> {
+  initialValues?: T[]
+  limit: number
+}
+
+export function useQueue<T>({ initialValues = [], limit }: useQueueArgs<T>) {
+  const [{ state, queue }, setState] = useState({
+    state: initialValues.slice(0, limit),
+    queue: initialValues.slice(limit),
+  })
+
+  const add = (...items: T[]) =>
+    setState((current) => {
+      const results = [...current.state, ...current.queue, ...items]
+
+      return {
+        state: results.slice(0, limit),
+        queue: results.slice(limit),
+      }
+    })
+
+  const update = (fn: (state: T[]) => T[]) =>
+    setState((current) => {
+      const results = fn([...current.state, ...current.queue])
+
+      return {
+        state: results.slice(0, limit),
+        queue: results.slice(limit),
+      }
+    })
+
+  const cleanQueue = () =>
+    setState((current) => ({ state: current.state, queue: [] }))
+
+  return {
+    state,
+    queue,
+    add,
+    update,
+    cleanQueue,
+  }
+}
