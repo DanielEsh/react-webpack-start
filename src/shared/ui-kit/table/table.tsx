4bebc881c179210fbeb824ae3074c8cf1f53e596
Daniel <daniilterawars@gmail.com>
2023-08-04 21:44:55 +0300

    feat(table): styles

--- src/shared/ui-kit/table/table.tsx
+++ src/shared/ui-kit/table/table.tsx
@@ -1,6 +1,11 @@
 import { TypeWithChildren } from 'shared/ui-kit/types'
+import { TableHead } from './table-head'
+import { TableBody } from './table-body'
+import { TableRow } from './table-row'
+import { TableColumnHeader } from './table-column-header'
+import { TableCell } from './table-cell'
 
-export const Table = ({ children }: TypeWithChildren) => {
+export const _Table = ({ children }: TypeWithChildren) => {
   return (
     <div className="rounded-md border">
       <div className="w-full overflow-auto">
@@ -9,3 +14,11 @@ export const Table = ({ children }: TypeWithChildren) => {
     </div>
   )
 }
+
+export const Table = Object.assign(_Table, {
+  Head: TableHead,
+  Body: TableBody,
+  Row: TableRow,
+  ColumnHeader: TableColumnHeader,
+  Cell: TableCell,
+})
