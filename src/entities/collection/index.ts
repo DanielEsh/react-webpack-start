b334c0ef2a8ffef3026e635c6a727a104846f94b
Daniel <daniilterawars@gmail.com>
2023-08-05 16:27:01 +0300

    feat(collection): remove old data

--- src/entities/collection/index.ts
+++ src/entities/collection/index.ts
@@ -1,16 +1,7 @@
 // ui
-import { CollectionsTable } from './ui/CollectionsTable'
-import { CollectionsTableHeader } from './ui/CollectionsTableHeader'
-import { CollectionsTableFooter } from './ui/CollectionsTableFooter'
 import { CollectionUpdateForm } from './ui/CollectionUpdateForm'
 
 // hooks
 import { useUpdateCollectionsList } from './hooks'
 
-export {
-  CollectionsTable,
-  CollectionsTableHeader,
-  CollectionsTableFooter,
-  CollectionUpdateForm,
-  useUpdateCollectionsList,
-}
+export { CollectionUpdateForm, useUpdateCollectionsList }
