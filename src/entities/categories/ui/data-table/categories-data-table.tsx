4abfd6255d88a21d5b415cba02a06d1a0ac625c9
Daniel <daniilterawars@gmail.com>
2023-08-19 00:37:03 +0300

    feat(categories): delete

--- src/entities/categories/ui/data-table/categories-data-table.tsx
+++ src/entities/categories/ui/data-table/categories-data-table.tsx
@@ -4,6 +4,11 @@ import { Category } from 'entities/categories/types'
 import { columns } from './columns'
 import { useDeleteConfirmation } from 'widgets/data-table/use-delete-confirmation'
 import { DeleteState } from 'widgets/data-table/model/delete'
+import {
+  useDeleteCategoryMutation,
+  useUpdateCategories,
+} from 'entities/categories/api/queries'
+import { useNotification } from 'shared/notification'
 
 interface Props {
   data: Category[]
@@ -16,9 +21,25 @@ export const CategoriesDataTable = ({ data }: Props) => {
     closeDeleteConfirmDialog,
     confirmDelete,
   } = useDeleteConfirmation()
+  const { mutate: deleteCategoryMutation } = useDeleteCategoryMutation()
+  const { updateCategories } = useUpdateCategories()
+  const { showNotification } = useNotification()
+
+  const handleSuccessCategoryDelete = (data: Category) => {
+    console.log('success delete', data)
+    showNotification({
+      id: '0',
+      title: 'Успешное удаление',
+      message: `message`,
+    })
+    updateCategories()
+  }
 
   const handleConfirmDelete = (data: DeleteState<number, Category>) => {
     console.log('DELETE', data)
+    deleteCategoryMutation(data.key, {
+      onSuccess: handleSuccessCategoryDelete,
+    })
   }
 
   return (
