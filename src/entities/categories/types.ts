a0543cc4d1622e2964ca3e0bfe281c9fceee4a5b
Daniel <daniilterawars@gmail.com>
2023-08-19 18:24:01 +0300

    feat(categories): dto

--- src/entities/categories/types.ts
+++ src/entities/categories/types.ts
@@ -1,3 +1,11 @@
+export interface CategoryDto {
+  slug: string
+  name: string
+  description: string
+  created_at: string
+  updated_at: string
+}
+
 export interface Category {
   id: number
   slug: string
