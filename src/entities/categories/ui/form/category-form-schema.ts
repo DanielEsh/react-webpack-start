b274ceef43bcca6a04902759703e613debd047c7
Daniel <daniilterawars@gmail.com>
2023-08-19 19:25:52 +0300

    feat(categories): slug validation

--- src/entities/categories/ui/form/category-form-schema.ts
+++ src/entities/categories/ui/form/category-form-schema.ts
@@ -1,9 +1,16 @@
 import { z } from 'zod'
 
+const slugRegex = new RegExp('^[a-z0-9]+(?:-[a-z0-9]+)*$')
+
 export const categoryFormSchema = z.object({
-  slug: z.string().nonempty({
-    message: 'Must be required',
-  }),
+  slug: z
+    .string()
+    .nonempty({
+      message: 'Must be required',
+    })
+    .refine((value) => slugRegex.test(value), {
+      message: 'Invalid slug format',
+    }),
   name: z.string().nonempty({
     message: 'Must be required',
   }),
