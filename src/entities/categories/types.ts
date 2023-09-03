eced3732fdc46e7c0e7d988ea786ebdd543066d8
Daniel <daniilterawars@gmail.com>
2023-09-03 09:37:18 +0300

    feat(products): types

--- src/entities/categories/types.ts
+++ src/entities/categories/types.ts
@@ -1,4 +1,5 @@
 export interface CategoryDto {
+  id: number
   slug: string
   name: string
   description: string
---
+++ src/entities/products/api/types.ts
@@ -0,0 +1,22 @@
+import type { BrandDto } from 'entities/brands/api/types'
+import type { CategoryDto } from 'entities/categories/types'
+
+type ProductBrand = Pick<BrandDto, 'id' | 'name' | 'slug'>
+type ProductCategory = Pick<CategoryDto, 'id' | 'name' | 'slug'>
+
+export interface ProductDto {
+  id: number
+  article: string
+  name: string
+  price: number
+  descriptions?: string
+  brand: ProductBrand
+  category: ProductCategory
+}
+
+export type CreateProductDto = Pick<
+  ProductDto,
+  'article' | 'name' | 'price' | 'brand' | 'category' | 'descriptions'
+>
+
+export type UpdateProductDto = CreateProductDto
