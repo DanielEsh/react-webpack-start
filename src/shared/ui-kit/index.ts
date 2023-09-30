839dd9e16791cc27266fdd688ebd4c61467ced08
Daniel <daniilterawars@gmail.com>
2023-09-30 18:39:32 +0300

    feat: shared

---
+++ src/shared/ui-kit/index.ts
@@ -0,0 +1,13 @@
+export * from './button'
+export * from './link'
+
+export * from './form'
+export * from './form-controls'
+
+export * from './divider'
+export * from './table'
+export * from './portal'
+
+export * from './modal'
+export * from './dialog'
+export * from './drawer'
