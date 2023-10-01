ba392383967f6d13e9c786aca5d3f23ca3aa5743
Daniel <daniilterawars@gmail.com>
2023-10-01 14:25:50 +0300

    feat(products-attributes-groups): table

--- src/entities/products/ui/product-attributes-groups.tsx
+++ src/entities/products/ui/product-attributes-groups.tsx
@@ -1,26 +1,97 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import { ReactNode } from 'react'
 import { Button, Table } from 'shared/ui-kit'
 
+interface ProductAttributesGroup {
+  name: string
+  count: number
+}
+
+const getColumns: ColumnDef<ProductAttributesGroup>[] = [
+  {
+    id: 'name',
+    accessorFn: ({ name }) => name,
+    header: () => (
+      <Table.ColumnHeader className="sticky top-0 min-w-[320px] bg-white">
+        Название
+      </Table.ColumnHeader>
+    ),
+    cell: (info) => <Table.Cell>{info.getValue() as ReactNode}</Table.Cell>,
+  },
+  {
+    id: 'count',
+    accessorFn: ({ count }) => count,
+    header: () => (
+      <Table.ColumnHeader className="sticky top-0 min-w-[320px] bg-white">
+        Количество
+      </Table.ColumnHeader>
+    ),
+    cell: (info) => <Table.Cell>{info.getValue() as ReactNode}</Table.Cell>,
+  },
+]
+
+const data: ProductAttributesGroup[] = [
+  {
+    name: 'Группа атрибутов 1',
+    count: 23,
+  },
+  {
+    name: 'Группа атрибутов 2',
+    count: 1,
+  },
+]
+
 export const ProductsAttributesGroups = () => {
+  const table = useReactTable({
+    data: data,
+    columns: getColumns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
   return (
     <div className="mt-4 px-6">
       <Table>
         <Table.Head>
-          <Table.Row>
-            <Table.ColumnHeader>Название</Table.ColumnHeader>
-            <Table.ColumnHeader>Количество</Table.ColumnHeader>
-          </Table.Row>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Table.Row key={headerGroup.id}>
+              {headerGroup.headers.map((header) =>
+                header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    ),
+              )}
+            </Table.Row>
+          ))}
         </Table.Head>
 
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>Группа атрибутов 1</Table.Cell>
-            <Table.Cell>20</Table.Cell>
-          </Table.Row>
-
-          <Table.Row>
-            <Table.Cell>Группа атрибутов 2</Table.Cell>
-            <Table.Cell>30</Table.Cell>
-          </Table.Row>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <Table.Row
+                key={row.id}
+                className="h-[54px]"
+              >
+                {row
+                  .getVisibleCells()
+                  .map((cell) =>
+                    flexRender(cell.column.columnDef.cell, cell.getContext()),
+                  )}
+              </Table.Row>
+            ))
+          ) : (
+            <Table.Row>
+              <Table.Cell className="h-[308px] text-center">
+                No results.
+              </Table.Cell>
+            </Table.Row>
+          )}
         </Table.Body>
       </Table>
 
