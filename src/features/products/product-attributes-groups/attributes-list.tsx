d8855e7514a6b53c1779c6b6497ee7e5c2e5a35d
Daniel <daniilterawars@gmail.com>
2023-10-02 22:00:32 +0300

    refactor(product-attribute-groups): attribute list

--- src/features/products/product-attributes-groups/attributes-list.tsx
+++ src/features/products/product-attributes-groups/attributes-list.tsx
@@ -8,16 +8,19 @@ interface Props {
 
 export const AttributesList = ({ attributes, onAddClick }: Props) => {
   return (
-    <div>
+    <div className="p-4">
       {attributes.map((attribute, index) => (
-        <div key={index}>
+        <div
+          className="flex w-full gap-[120px]"
+          key={index}
+        >
           <div>name: {attribute.name}</div>
           <div>value: {attribute.value}</div>
         </div>
       ))}
 
       <Button
-        variant="ghost"
+        size="xs"
         onClick={onAddClick}
       >
         Связать с атрибутом
