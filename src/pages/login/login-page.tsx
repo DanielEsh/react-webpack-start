db1abc2524065534766392f35d49f38b55ce3086
Daniel <daniilterawars@gmail.com>
2023-10-14 20:08:49 +0300

    feat(auth): login

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,3 +1,20 @@
+import { Button, Input } from 'shared/ui-kit'
+
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  return (
+    <div className="flex h-screen w-full">
+      <aside className="h-full p-[120px]">
+        <div className="flex w-[360px] flex-col gap-4">
+          <h1>Login Page</h1>
+          <Input label="name" />
+          <Input label="password" />
+
+          <Button variant="primary">Submit</Button>
+        </div>
+      </aside>
+      <div className="h-full w-full p-6">
+        <div className="h-full w-full rounded-lg bg-gray-900"></div>
+      </div>
+    </div>
+  )
 }
