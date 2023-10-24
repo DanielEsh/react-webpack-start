5163f0fdaa79f936c8fad02b582a0d2bf6a9542e
Daniel <daniilterawars@gmail.com>
2023-10-24 19:16:27 +0300

    feat(warehouse): dto

---
+++ src/entities/warehouse/api/dto.ts
@@ -0,0 +1,13 @@
+interface WarehouseProductsDto {
+  productId: number
+  quantity: number
+}
+
+export interface WarehouseDto {
+  id: number
+  name: string
+  products: WarehouseProductsDto[]
+}
+
+export type WarehouseCreateDto = Pick<WarehouseDto, 'name' | 'products'>
+export type WarehouseUpdateDto = Pick<WarehouseDto, 'id' | 'name' | 'products'>
