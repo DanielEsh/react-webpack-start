bdbe7655ce8aeaf0b97e58c31cce3f37d32235b4
Daniel <daniilterawars@gmail.com>
2023-11-16 22:14:16 +0300

    feat(order): order layout

--- src/pages/order/order-details-page.tsx
+++ src/pages/order/order-details-page.tsx
@@ -1,8 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom'
-import { Button, Drawer } from 'shared/ui-kit'
+import { Accordion, Button, Drawer } from 'shared/ui-kit'
 import { DrawerHeader } from 'shared/ui-kit/drawer/drawer-header'
 import { DrawerFooter } from 'shared/ui-kit/drawer/drawer-footer'
 import { useGetOrderByIdQuery } from 'entities/order/api/queries/use-get-order-by-id-query'
+import { AccordionDemo } from 'pages/sandbox/ui/accordion-demo'
 
 export default function OrderDetailsPage() {
   const { id } = useParams<{
@@ -27,29 +28,71 @@ export default function OrderDetailsPage() {
         <h2>Order details</h2>
       </DrawerHeader>
 
-      <div className="p-4">orders</div>
+      <div className="p-4">
+        <div>status:</div>
+        <div>payment_type</div>
+        <div>payment_status</div>
+        <div>manager:</div>
+        <div>warehouse:</div>
+      </div>
 
       {isSuccess && (
         <>
-          <div>
-            <h2>User Data</h2>
-            <div>name: {data.user_details.name}</div>
-            <div>name: {data.user_details.phone}</div>
-            <div>name: {data.user_details.email}</div>
-            <div>name: {data.user_details.comment}</div>
-          </div>
-          <div>
-            <h2>Delivery Data</h2>
-            <div>city:</div>
-            <div>street:</div>
-            <div>house:</div>
-            <div>building:</div>
-            <div>ApartmentOffice:</div>
-            <div>index:</div>
-          </div>
-          <div>
-            <h2>Products List</h2>
-          </div>
+          <Accordion
+            type="multiple"
+            className="w-full"
+            defaultValue={['userDetails', 'deliveryDetails']}
+          >
+            <Accordion.Item value="userDetails">
+              <div className="p-4">
+                <Accordion.Trigger>
+                  <h2>User details</h2>
+                </Accordion.Trigger>
+              </div>
+
+              <Accordion.Content>
+                <div className="p-4">
+                  <div>firstName: test</div>
+                  <div>lastName: test</div>
+                  <div>middleName: test</div>
+                  <div>phone: {data.user_details.phone}</div>
+                  <div>email: {data.user_details.email}</div>
+                  <div>guest: test</div>
+                  <div>comment: {data.user_details.comment}</div>
+                </div>
+              </Accordion.Content>
+            </Accordion.Item>
+
+            <Accordion.Item value="deliveryDetails">
+              <div className="p-4">
+                <Accordion.Trigger>
+                  <h2>delivery details</h2>
+                </Accordion.Trigger>
+              </div>
+              <Accordion.Content>
+                <div className="p-4">
+                  <div>Country: </div>
+                  <div>City: </div>
+                  <div>Street: </div>
+                  <div>House: </div>
+                  <div>Building: </div>
+                  <div>ApartmentOffice:</div>
+                  <div>ZipCode:</div>
+                </div>
+              </Accordion.Content>
+            </Accordion.Item>
+
+            <Accordion.Item value="orderProducts">
+              <div className="p-4">
+                <Accordion.Trigger>
+                  <h2>products</h2>
+                </Accordion.Trigger>
+              </div>
+              <Accordion.Content>
+                <div className="p-4">ProductsTable</div>
+              </Accordion.Content>
+            </Accordion.Item>
+          </Accordion>
         </>
       )}
 
